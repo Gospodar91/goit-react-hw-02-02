9ed00e907e0a1210ad371e3dbdbc0e615e9e175d
Sergei <xsergeix@ukr.net>
2020-03-12 22:14:54 +0300

    Updates

---
+++ precache-manifest.16d35fd9cdec1bea41ff7e9c84561235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a60cbe4b05c683ef68c9efff0cb72e",
+    "url": "/goit-react-hw-02-02/index.html"
+  },
+  {
+    "revision": "2132e151558b3ac88dbe",
+    "url": "/goit-react-hw-02-02/static/css/main.0f89ed29.chunk.css"
+  },
+  {
+    "revision": "9230f39cb038d25d95d0",
+    "url": "/goit-react-hw-02-02/static/js/2.bcd2075b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-02/static/js/2.bcd2075b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2132e151558b3ac88dbe",
+    "url": "/goit-react-hw-02-02/static/js/main.801d8b76.chunk.js"
+  },
+  {
+    "revision": "0eb4569da6cca52a7492",
+    "url": "/goit-react-hw-02-02/static/js/runtime-main.4696c751.js"
+  }
+]);
